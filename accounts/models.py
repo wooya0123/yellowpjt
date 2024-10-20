ab9d0d902b13f9a1038984522ca87cfe6aa8af66
unknown <cakejuuu@gmail.com>
2024-10-21 00:17:17 +0900

    모델 수정

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
-    password = models.CharField(max_length=30)
-    last_login = models.DateTimeField(auto_now=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.CharField(max_length=50, unique=True)
-    is_superuser = models.BooleanField()
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
